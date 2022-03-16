b762fad30898b7addb99357376329d81a8570684
Henry Salliss <henrysalliss@gmail.com>
2022-03-16 18:30:54 +0000

    changed state store from redux to react context

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useContext } from "react";
+import { useSelector } from "react-redux";
 
-import ProductItem from '../components/Products/ProductItem';
-import './Products.css';
+import ProductItem from "../components/Products/ProductItem";
+import "./Products.css";
 
-const Products = props => {
-  const productList = useSelector(state => state.shop.products);
+import { ProductContext } from "../context/product-context";
+
+const Products = (props) => {
+  const productList = useContext(ProductContext).products;
   return (
     <ul className="products-list">
-      {productList.map(prod => (
+      {productList.map((prod) => (
         <ProductItem
           key={prod.id}
           id={prod.id}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import ProductProvider from "./context/product-context";
 
-import './index.css';
-import App from './App';
-import productReducer from './store/reducers/products';
+// redux imports
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
+// import { Provider } from "react-redux";
+// import { combineReducers, createStore } from "redux";
 
-const store = createStore(rootReducer);
+import "./index.css";
+import App from "./App";
+// import productReducer from "./store/reducers/products";
 
 ReactDOM.render(
-  <Provider store={store}>
+  <ProductProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
+  </ProductProvider>,
+  document.getElementById("root")
 );
